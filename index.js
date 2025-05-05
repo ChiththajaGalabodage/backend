538e249233b8bb8cda362ef0e1d425c9b9211d0c
Chiththaja <162040367+ChiththajaGalabodage@users.noreply.github.com>
2025-05-05 09:07:10 +0530

    env implementation  ⚙️

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/userRoute.js";
 import jwt from "jsonwebtoken";
 import orderRouter from "./routes/orderRoute.js";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -36,9 +39,7 @@ app.use((req, res, next) => {
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:123@cluster0.jcvuatc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Connected to the database");
   })
